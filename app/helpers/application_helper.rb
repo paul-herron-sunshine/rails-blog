8323ee03cf4e8d559657344618366e7c35283267
Fraser Johnstone <fraser.johnstone@onthebeach.co.uk>
2018-10-16 20:51:00 +0100

    Made the static home and about pages a little less static by adding a helper to create the page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = "OTB Academy Blog 2018"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
