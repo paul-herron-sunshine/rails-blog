0b5900007365b3a7a198d596e3ebf350504dbd1f
Fraser Johnstone <fraser.johnstone@onthebeach.co.uk>
2018-10-17 13:45:07 +0100

    finished sign up, tarting sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Account Created! Welcome to the OTB Academy Blog"
       redirect_to @user
     else
       render 'new'
