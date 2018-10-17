58c7f409a1ff80c47f23e66eb1e5d950ea402d85
Fraser Johnstone <fraser.johnstone@onthebeach.co.uk>
2018-10-17 03:38:46 +0100

    added the signup page and users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+
   root 'static_pages#home'
-  get 'static_pages/about'
-  get 'static_pages/home'
+  get '/home', to: 'static_pages#home'
+  get '/about', to: 'static_pages#about'
+  get '/signup', to: 'users#new'
 end
