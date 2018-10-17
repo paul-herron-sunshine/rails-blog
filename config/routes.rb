6cca4513113eca9c5f433bb824daacd9008e8c40
Paul Herron <paul.herron@onthebeach.co.uk>
2018-10-17 15:26:50 +0100

    Comment section - removed files and restarted. Initial pages created with comment output.

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
 
-<<<<<<< HEAD
   get 'comments/new'
   root 'static_pages#home'
   get '/home', to: 'static_pages#home'
   get '/about', to: 'static_pages#about'
   get '/signup', to: 'users#new'
   post '/signup',  to: 'users#create'
-=======
+
   root "posts#index"
->>>>>>> 383f792b941ee0e060cf1a985eef30f53e8902b1
 
   # get 'home', to: 'static_pages#home'
   get 'about' => 'pages#about'
@@ -18,5 +16,6 @@ Rails.application.routes.draw do
 
   resources :users
   resources :posts
+  resources :comments
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,26 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2018_10_17_105831) do
+ActiveRecord::Schema.define(version: 2018_10_17_135807) do
 
   create_table "comments", force: :cascade do |t|
-    t.text "content"
+    t.text "body"
     t.integer "user_id"
-    t.integer "blogpost_id"
+    t.integer "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["blogpost_id"], name: "index_comments_on_blogpost_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
-=======
-ActiveRecord::Schema.define(version: 2018_10_17_105915) do
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
->>>>>>> 383f792b941ee0e060cf1a985eef30f53e8902b1
   end
 
   create_table "users", force: :cascade do |t|
