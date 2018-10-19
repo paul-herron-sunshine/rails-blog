c2f523f91053c819be675959e40f1596ffa3af42
kirstiedavidson <kjd35uk@yahoo.co.uk>
2018-10-19 11:32:43 +0100

    Delete post function works and redirects to homepage

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
   include SessionsHelper
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.all
   end
@@ -14,14 +16,28 @@ class PostsController < ApplicationController
   def create
     # render plain: params[:post].inspect
     @post = Post.new(post_params)
-
     @post.save
     #loads show view:
     redirect_to @post
   end
 
-  private def post_params
+  def destroy
+        # destroy method deletes record from db
+    @post.destroy
+    respond_to do |format|
+      format.html { redirect_to posts_url, notice: 'Your post was successfully deleted.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private 
+  
+  def post_params
     params.require(:post).permit(:user_id, :title, :body)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 
 end
