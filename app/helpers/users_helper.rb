d1653388137cd8c83b46237308fb42c8083f8a9a
Fraser Johnstone <fraser.johnstone@onthebeach.co.uk>
2018-10-21 10:19:24 +0100

    added functionality to detect last login time. ordered user search resuts by views. Placed currently online users to the top of the search lists

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,4 +4,9 @@ module UsersHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
+
+  def days_since_login(user)
+    #(Time.now - user.last_active_at) / 8640
+    distance_of_time_in_words(user.last_active_at, Time.now)
+  end
 end
